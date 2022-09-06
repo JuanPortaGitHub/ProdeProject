6a7ce32df1d1d1531620a8fdcec783106537e672
Juan Porta <jporta@serv.externos.prismamp.com>
2022-09-06 15:28:20 -0300

    testing before remove Nexus

---
+++ prisma/migrations/20220906175059_remove_field_in_users_by_group/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Prode_Partido_UsuarioInfo_PartidosId` on the `Usuarios_Por_Grupo` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Usuarios_Por_Grupo" DROP COLUMN "Prode_Partido_UsuarioInfo_PartidosId";
