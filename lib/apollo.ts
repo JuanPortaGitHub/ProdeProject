569fea93407ef353fb7522a4be30210f11a6cefc
Juan Porta <jporta@serv.externos.prismamp.com>
2022-09-28 12:59:15 -0300

    fix for apollo client

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { relayStylePagination } from "@apollo/client/utilities";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: process.env.URL_BASE + "/api/graphql",
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
