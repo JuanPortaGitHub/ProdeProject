dc6fa550f62b26acf2b3705863bab2c621e82f57
Juan Porta <jporta@serv.externos.prismamp.com>
2022-10-09 14:49:09 -0300

    fix in try catch

--- graphql/resolvers/gruposResolvers.ts
+++ graphql/resolvers/gruposResolvers.ts
@@ -30,34 +30,30 @@ export const createGrupoResolver: FieldResolver<
   { nombre, imagen, clave_grupo, slogan, monto, idUser },
   { prisma }
 ) => {
-  try {
-    const nombreGrupoExist = await prisma.grupo.count({
-      where: {
-        nombre: nombre,
-      },
-    });
-    if (nombreGrupoExist !== 0) {
-      throw new Error("Ese nombre de grupo ya esta siendo utilizado");
-    }
-    const newGroup = await prisma.grupo.create({
-      data: {
-        nombre: nombre.toLowerCase(),
-        imagen,
-        clave_grupo: clave_grupo.toLowerCase(),
-        slogan,
-        monto,
-        UsuariosDeGrupo: {
-          connectOrCreate: {
-            where: { id: idUser },
-            create: { id: idUser },
-          },
+  const nombreGrupoExist = await prisma.grupo.count({
+    where: {
+      nombre: nombre,
+    },
+  });
+  if (nombreGrupoExist !== 0) {
+    throw new Error("Ese nombre de grupo ya esta siendo utilizado");
+  }
+  const newGroup = await prisma.grupo.create({
+    data: {
+      nombre: nombre.toLowerCase(),
+      imagen,
+      clave_grupo: clave_grupo.toLowerCase(),
+      slogan,
+      monto,
+      UsuariosDeGrupo: {
+        connectOrCreate: {
+          where: { id: idUser },
+          create: { id: idUser },
         },
       },
-    });
-    return newGroup;
-  } catch {
-    throw new Error("No se pudo crear grupo, reintente");
-  }
+    },
+  });
+  return newGroup;
 };
 
 export const updateGrupoResolver: FieldResolver<
@@ -68,32 +64,27 @@ export const updateGrupoResolver: FieldResolver<
   { id, nombre, imagen, clave_grupo, slogan, monto },
   { prisma }
 ) => {
-  try {
-    if (nombre) {
-      const nombreGrupoExist = await prisma.grupo.count({
-        where: {
-          nombre: nombre.toLowerCase(),
-        },
-      });
-      if (nombreGrupoExist !== 0) {
-        throw new Error("Ese nombre de grupo ya esta siendo utilizado");
-      }
-    }
-    const editedGroup = await prisma.grupo.update({
-      where: { id: id },
-      data: {
-        nombre: nombre != null ? nombre.toLowerCase() : undefined, //para que funcione como patch
-        imagen: imagen != null ? imagen : undefined, //para que funcione como patch
-        clave_grupo:
-          clave_grupo != null ? clave_grupo.toLowerCase() : undefined, //para que funcione como patch
-        slogan: slogan != null ? slogan : undefined, //para que funcione como patch
-        monto: monto != null ? monto : undefined, //para que funcione como patch
+  if (nombre) {
+    const nombreGrupoExist = await prisma.grupo.count({
+      where: {
+        nombre: nombre.toLowerCase(),
       },
     });
-    return editedGroup;
-  } catch {
-    throw new Error("No se pudo actualizar grupo, reintente");
+    if (nombreGrupoExist !== 0) {
+      throw new Error("Ese nombre de grupo ya esta siendo utilizado");
+    }
   }
+  const editedGroup = await prisma.grupo.update({
+    where: { id: id },
+    data: {
+      nombre: nombre != null ? nombre.toLowerCase() : undefined, //para que funcione como patch
+      imagen: imagen != null ? imagen : undefined, //para que funcione como patch
+      clave_grupo: clave_grupo != null ? clave_grupo.toLowerCase() : undefined, //para que funcione como patch
+      slogan: slogan != null ? slogan : undefined, //para que funcione como patch
+      monto: monto != null ? monto : undefined, //para que funcione como patch
+    },
+  });
+  return editedGroup;
 };
 
 export const addUserToGrupo: FieldResolver<"Mutation", "updateGrupo"> = async (
@@ -101,49 +92,45 @@ export const addUserToGrupo: FieldResolver<"Mutation", "updateGrupo"> = async (
   { nombre, clave_grupo, idUser },
   { prisma }
 ) => {
-  try {
-    if (nombre && clave_grupo && idUser) {
-      const nombreGrupoExist = await prisma.grupo.count({
-        where: {
-          AND: [
-            {
-              nombre: { equals: nombre, mode: "insensitive" },
-            },
-            {
-              clave_grupo: { equals: clave_grupo, mode: "insensitive" },
-            },
-          ],
-        },
-      });
-      if (nombreGrupoExist === 0) {
-        throw new Error("No se encuentra el grupo o clave incorrecta");
-      }
-      const userExistInGroup = await prisma.grupo.count({
-        where: {
-          AND: [
-            {
-              nombre: { equals: nombre, mode: "insensitive" },
-            },
-            {
-              UsuariosDeGrupo: { some: { id: idUser } },
-            },
-          ],
-        },
-      });
-      if (userExistInGroup !== 0) {
-        throw new Error("Ya estás en este grupo!");
-      }
-      const editedGroup = await prisma.grupo.update({
-        where: { nombre: nombre },
-        data: {
-          UsuariosDeGrupo: {
-            connect: { id: idUser },
+  if (nombre && clave_grupo && idUser) {
+    const nombreGrupoExist = await prisma.grupo.count({
+      where: {
+        AND: [
+          {
+            nombre: { equals: nombre, mode: "insensitive" },
           },
-        },
-      });
-      return editedGroup;
+          {
+            clave_grupo: { equals: clave_grupo, mode: "insensitive" },
+          },
+        ],
+      },
+    });
+    if (nombreGrupoExist === 0) {
+      throw new Error("No se encuentra el grupo o clave incorrecta");
     }
-  } catch {
-    throw new Error("No se pudo agregar usuario. Reintente");
+    const userExistInGroup = await prisma.grupo.count({
+      where: {
+        AND: [
+          {
+            nombre: { equals: nombre, mode: "insensitive" },
+          },
+          {
+            UsuariosDeGrupo: { some: { id: idUser } },
+          },
+        ],
+      },
+    });
+    if (userExistInGroup !== 0) {
+      throw new Error("Ya estás en este grupo!");
+    }
+    const editedGroup = await prisma.grupo.update({
+      where: { nombre: nombre },
+      data: {
+        UsuariosDeGrupo: {
+          connect: { id: idUser },
+        },
+      },
+    });
+    return editedGroup;
   }
 };
